e0156828dedf4d5d2c3e4044721d316e384fec00
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-13 20:38:49 +0200

    init proj

--- AddPullToRefreshInUITableView/ViewController.swift
+++ AddPullToRefreshInUITableView/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.myTableView.delegate = self
+        self.myTableView.dataSource = self
     }
-
-
+    
+    var dataStore = ["Mobile Development", "Web Design", "UI/UX", "Python"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataStore.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let foodElement = dataStore[indexPath.row]
+        
+        cell.textLabel?.text = foodElement
+        
+        return cell
+    }
 }
 
