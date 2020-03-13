a17f306cb4d3f4e09a63e1da77c2b62bfa8a5f60
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-13 20:30:09 +0200

    Initial Commit

---
+++ AddPullToRefreshInUITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AddPullToRefreshInUITableView
+//
+//  Created by ramil on 13.03.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
